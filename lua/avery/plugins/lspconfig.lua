aba9edce07847e0e5e210cd2e3fbf07f6286cba3
Avery Lin <100400733+ItsSunnyMonster@users.noreply.github.com>
2026-08-07 17:19:18 +1200

    fix deprecation

--- lua/avery/plugins/lspconfig.lua
+++ lua/avery/plugins/lspconfig.lua
@@ -16,7 +16,7 @@ return {
         { "<c-k>", function() return vim.lsp.buf.signature_help() end, mode = "i", desc = "Signature Help" },
         { "<leader>ca", vim.lsp.buf.code_action, desc = "Code Action", mode = { "n", "x" } },
         { "<leader>cc", vim.lsp.codelens.run, desc = "Run Codelens", mode = { "n", "x" } },
-        { "<leader>cC", vim.lsp.codelens.refresh, desc = "Refresh & Display Codelens", mode = { "n" } },
+        { "<leader>cC", vim.lsp.codelens.on_refresh, desc = "Refresh & Display Codelens", mode = { "n" } },
         { "<leader>cr", vim.lsp.buf.rename, desc = "Rename" },
     },
 }
